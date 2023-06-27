1c379c2f6ecd71844a8a763511db214a3a433fe8
akhilmondal <akhilmandalfamily@gmail.com>
2023-06-27 08:57:44 +0530

    [AKhil] Added: functionality to request forget password by user, also added functionality to reset the password.

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -3,8 +3,7 @@ dotenv.config();
 import User from '../models/user.model';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-
-
+import * as utils from '../utils/sendmail.util';
 
 //create new user
 export const newUser = async (body) => {
@@ -26,7 +25,11 @@ export const userLogin = async (body) => {
   const data = await User.findOne({ emailId: body.emailId });
   if (data) {
     if (bcrypt.compareSync(body.passWord, data.passWord)) {
-      var token = jwt.sign({ id: data.id, emailId: data.emailId }, process.env.SECRET_KEY, {expiresIn:'3h'});
+      var token = jwt.sign(
+        { id: data.id, emailId: data.emailId },
+        process.env.SECRET_KEY,
+        { expiresIn: '3h' }
+      );
       return token;
     } else {
       throw new Error('Invalid Password.');
@@ -34,4 +37,44 @@ export const userLogin = async (body) => {
   } else {
     throw new Error('Invalid emailId.');
   }
+};
+
+// Forget PassWord
+export const forgetPassWord = async (body) => {
+  const data = await User.findOne({ emailId: body.emailId });
+  if (data) {
+    const token = jwt.sign(
+      {
+        emailId: data.emailId,
+        _id: data._id
+      },
+      process.env.PASSWORD_RESET_KEY,
+      { expiresIn: '3h' }
+    );
+    await utils.sendMail(data.emailId, token);
+    return token;
+  } else {
+    throw new Error('Email id not found.');
+  }
+};
+
+// Service for reset password
+export const resetPassWord = async (_id, body) => {
+  const saltRounds = 10;
+  const salt = bcrypt.genSaltSync(saltRounds);
+  const hash = bcrypt.hashSync(body.passWord, salt);
+  body.passWord = hash;
+  const data = await User.findByIdAndUpdate(
+    { _id: _id },
+    { passWord: body.passWord },
+    {
+      new: true
+    }
+  );
+  if (!data) {
+    throw new Error('Invalid user id.');
+  } else {
+    return data;
+  }
+
 };
