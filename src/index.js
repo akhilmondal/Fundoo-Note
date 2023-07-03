3a4f8756302b965d3c22f6f33d6871eaf262eae7
akhilmondal <akhilmandalfamily@gmail.com>
2023-07-03 14:49:43 +0530

    [Akhil] Refactored: Imported Redis and initialised from index.js to start connection with redis database.

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import helmet from 'helmet';
 
 import routes from './routes';
 import database from './config/database';
+import redis from './config/redis';
 import {
   appErrorHandler,
   genericErrorHandler,
@@ -34,6 +35,7 @@ app.use(morgan('combined', { stream: logStream }));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 database();
+redis();
 
 app.use(`/api/${api_version}`, routes()); // To go for the requested routes './routes/index.js'
 app.use(appErrorHandler);
